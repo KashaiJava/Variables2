24e61b7376a4aabfa54b829df78c9909de6bb905
Nikita <fatyanov_n@mail.ru>
2022-08-27 15:16:11 +0400

    Первый коммит

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int weight = 500;
+        byte hight = 30;
+        short c = 10;
+        long a = 1000L;
+        float d = 1.5f;
+        double g = 2.045667886;
+        boolean pig = true;
+        System.out.println("Вес " + weight);
+        System.out.println("Высота " + hight);
+        System.out.println("С= " + c);
+        System.out.println("a= " + a);
+        System.out.println("d= " + d);
+        System.out.println("g= " + g);
+        System.out.println("Pig= " + pig);
+    }
+}
